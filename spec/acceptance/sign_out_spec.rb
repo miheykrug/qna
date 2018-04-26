26e35a177b8bb73975613a75c78c0e9139c1be6e
Mikhail Kruglik <miheykrug@mail.ru>
2018-04-26 21:36:24 +0300

    Lesson 5: user can log out

---
+++ spec/acceptance/sign_out_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+feature 'User sign out', %q{
+  In order to be able to end session
+  As an authenticated user
+  I want to be able to sign out
+} do
+  given(:user) { create(:user) }
+  scenario 'Authenticated user sign out' do
+    sign_in(user)
+
+    click_on 'Log out'
+
+    expect(page).to have_content 'Signed out successfully.'
+    expect(current_path).to eq root_path
+  end
+
+  scenario 'Non-authenticated user sign out' do
+    visit root_path
+    expect(page).to_not have_content 'Log out'
+  end
+end
