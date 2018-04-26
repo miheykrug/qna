91cc8d8deb19a88c65dec56a41e60d071a75a3ca
Mikhail Kruglik <miheykrug@mail.ru>
2018-04-26 22:12:53 +0300

    Lesson 5: user can sign up

---
+++ spec/acceptance/sign_up_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+feature 'User sign up', %q{
+  In order to be able to sign in
+  As an user
+  I want to be able to sign up
+} do
+
+  given(:registered_user) { create(:user) }
+  given(:non_registered_user) { build(:user) }
+
+  scenario 'Non-registered user try to sign up' do
+    sign_up(non_registered_user)
+
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
+    expect(current_path).to eq root_path
+  end
+
+  scenario 'Registered user try to sign up' do
+    sign_up(registered_user)
+
+    expect(page).to have_content 'Email has already been taken'
+    expect(current_path).to eq '/users'
+
+  end
+
+  scenario 'Authenticated user try to sign up' do
+    sign_in(registered_user)
+    visit new_user_registration_path
+
+    expect(page).to have_content 'You are already signed in.'
+    expect(current_path).to eq root_path
+  end
+end
--- spec/support/acceptance_macros.rb
+++ spec/support/acceptance_macros.rb
@@ -11,4 +11,12 @@ module AcceptanceMacros
     fill_in 'Body', with: 'My Answer'
     click_on 'Give answer'
   end
+
+  def sign_up(user)
+    visit new_user_registration_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Password confirmation', with: user.password_confirmation
+    click_on 'Sign up'
+  end
 end
