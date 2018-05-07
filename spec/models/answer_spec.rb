1ef6bf9ff2681e0160491b55ac2c123e3baa288e
Mikhail Kruglik <miheykrug@mail.ru>
2018-05-08 00:29:06 +0300

    Lesson 8: user can add files to question and answer (screencast code)

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -5,6 +5,9 @@ RSpec.describe Answer, type: :model do
 
   it { should belong_to(:question) }
   it { should belong_to(:user) }
+  it { should have_many(:attachments).dependent(:destroy) }
+
+  it { should accept_nested_attributes_for :attachments }
 
   describe '#set_best_question' do
     let(:question) { create(:question) }
