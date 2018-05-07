a0acf6a737b1b9f7b48c3f6e8a33b5911886c5e4
Mikhail Kruglik <miheykrug@mail.ru>
2018-05-07 16:06:26 +0300

    Lesson 7: fix some mistakes

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -5,4 +5,22 @@ RSpec.describe Answer, type: :model do
 
   it { should belong_to(:question) }
   it { should belong_to(:user) }
+
+  describe '#set_best_question' do
+    let(:question) { create(:question) }
+    let!(:old_best_answer) { create(:answer, question: question, best: true) }
+    let(:new_best_answer) { create(:answer, question: question) }
+
+    before { new_best_answer.set_best_answer }
+
+    it 'set new best answer of question' do
+      new_best_answer.reload
+      expect(new_best_answer).to be_best
+    end
+
+    it 'set old best answer best attribute to false' do
+      old_best_answer.reload
+      expect(old_best_answer).to_not be_best
+    end
+  end
 end
