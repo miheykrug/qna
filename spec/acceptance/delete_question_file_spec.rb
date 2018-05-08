a3676884f55a560b6f8251d66b5810b9158f023f
Mikhail Kruglik <miheykrug@mail.ru>
2018-05-08 21:35:30 +0300

    Lesson 8: author can delete his question attachment

---
+++ spec/acceptance/delete_question_file_spec.rb
@@ -0,0 +1,35 @@
+require_relative 'acceptance_helper'
+
+feature 'Delete question file', %q{
+  In order to remove unnecessary attachment
+  As an question's author
+  I'd  like to be able to delete file
+} do
+
+  given(:author) {  create(:user) }
+  given(:user) {  create(:user) }
+  given(:question) { create(:question, user: author) }
+  given!(:file) { create(:attachment, attachable: question)}
+
+  describe 'Authenticate user' do
+    scenario 'try delete file of his question', js: true do
+      sign_in(author)
+      visit question_path(question)
+
+      click_on 'Delete file'
+      page.driver.browser.switch_to.alert.accept
+
+      expect(page).to_not have_link 'rails_helper.rb'
+    end
+    scenario 'try delete file of another user question' do
+      sign_in(user)
+      visit question_path(question)
+      expect(page).to_not have_link 'Delete file'
+    end
+  end
+
+  scenario 'Non-authenticate user try delete file' do
+    visit question_path(question)
+    expect(page).to_not have_link 'Delete file'
+  end
+end
--- spec/factories/attachments.rb
+++ spec/factories/attachments.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :attachment do
-    file "MyString"
+    file { Rack::Test::UploadedFile.new("#{Rails.root}/spec/rails_helper.rb") }
   end
 end
