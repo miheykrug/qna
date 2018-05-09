b18f8201f88c3819afd1cc31a059034a846609fd
Mikhail Kruglik <miheykrug@mail.ru>
2018-05-09 15:05:51 +0300

    Lesson 8: add scenario tests to attach more than one file

--- spec/acceptance/add_files_to_answers_spec.rb
+++ spec/acceptance/add_files_to_answers_spec.rb
@@ -25,4 +25,22 @@ feature 'Add files to answer', %q{
       expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/1/rails_helper.rb'
     end
   end
+
+  scenario 'User adds more than one file', js: true do
+    within('.answer-form') do
+      fill_in 'Body', with: 'My Answer'
+      click_on 'add file'
+    end
+
+    inputs = all('input[type="file"]')
+    inputs[0].set("#{Rails.root}/spec/rails_helper.rb")
+    inputs[1].set("#{Rails.root}/spec/spec_helper.rb")
+
+    click_on 'Give answer'
+
+    within('.answers') do
+      expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/1/rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/2/spec_helper.rb'
+    end
+  end
 end
