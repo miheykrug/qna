e6e99d1b39891df21cf6e9a3575c400f0008dcd7
Mikhail Kruglik <miheykrug@mail.ru>
2018-06-21 16:44:43 +0300

    Lesson 17: fix some mistakes

--- app/jobs/new_answer_notify_job.rb
+++ app/jobs/new_answer_notify_job.rb
@@ -3,8 +3,8 @@ class NewAnswerNotifyJob < ApplicationJob
 
   def perform(object)
     users = object.question.subscribers
-    users.each do |user|
-      AnswerMailer.new_notify(user, object.question).deliver_now
+    users.find_each do |user|
+      AnswerMailer.new_notify(user, object.question).deliver_later
     end
   end
 end
