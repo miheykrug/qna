8b8dfd6f62f206d8ccf77b88735716be9ab094c1
Mikhail Kruglik <miheykrug@mail.ru>
2018-06-22 10:03:54 +0300

    Lesson 18: add indices to answer, comment and user

--- app/indices/answer_index.rb
+++ app/indices/answer_index.rb
@@ -1,6 +1,5 @@
-ThinkingSphinx::Index.define :question, with: :active_record do
+ThinkingSphinx::Index.define :answer, with: :active_record do
   # fields
-  indexes title, sortable: true
   indexes body
   indexes user.email, as: :author, sortable: true
 
