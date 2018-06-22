30197f28c6247199fded8d1e436237934b223586
Mikhail Kruglik <miheykrug@mail.ru>
2018-06-22 16:53:26 +0300

    Lesson 18: sphinx test work with question

--- spec/acceptance/search_spec.rb
+++ spec/acceptance/search_spec.rb
@@ -2,15 +2,23 @@ require_relative 'acceptance_helper'
 
 feature 'Search in questions, answers, comments and users' do
   given!(:user) { create(:user) }
+  given!(:question) { create(:question) }
 
-  scenario 'try to search user by email', js: true, sphinx: true do
-    ThinkingSphinx::Test.run do
+  # scenario 'try to search user by email', js: true, sphinx: true do
+  #     visit queries_path
+  #     fill_in 'Query', with: user.email
+  #     click_button 'Search'
+  #
+  #     expect(page).to have_content user.email
+  #     expect(page).to have_content user.id
+  # end
+
+  scenario 'try to search question by title', js: true, sphinx: true do
       visit queries_path
-      fill_in 'Query', with: user.email
+      fill_in 'Query', with: question.title
       click_button 'Search'
 
-      expect(page).to have_content user.email
-      expect(page).to have_content user.id
-    end
+      expect(page).to have_content question.title
+      expect(page).to have_content question.body
   end
 end
