de696d5f7e61adabd8b46c8e834e939bd4a993f1
Mikhail Kruglik <miheykrug@mail.ru>
2018-06-26 17:47:56 +0300

    Lesson 20: change deploy.rb - add config/master.key

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,7 +18,7 @@ set :rvm_type, :system
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml"
+append :linked_files, "config/database.yml", "config/master.key"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
